c7f95d86d114d8f49328e0047aa44e8669813413
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-05-04 12:37:17 +0100

    updates

--- get_data.ps1
+++ get_data.ps1
@@ -2,30 +2,43 @@
 
 # set this to whatever your Gateway's IP address is
 $ipaddr="192.168.1.135"
+# obtain an API key from https://entrez.enphaseenergy.com/ using your Enphase login details
+$apikey="topsecretkey"
 
 echo "info"
-curl -f -k -H 'Accept: application/json' -H 'Authorization: Bearer eyJraWQiOiI3ZDEwMDA1ZC03ODk5LTRkMGQtYmNiNC0yNDRmOThlZTE1NmIiLCJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJhdWQiOiI0ODI0MzcwMDczMDAiLCJpc3MiOiJFbnRyZXoiLCJlbnBoYXNlVXNlciI6Im93bmVyIiwiZXhwIjoxODAyMTkwMTE5LCJpYXQiOjE3NzA2NTQxMTksImp0aSI6ImNlZTFhZjI0LTU1MzQtNDZkYi04ZWM0LTNlZWVlMDM0YTgxNiIsInVzZXJuYW1lIjoibWFya21jaW50eXJlOTlAZ29vZ2xlbWFpbC5jb20ifQ.SJeIHOLW1B96ejeDimbR8yMCMcNhPTF6vLaIulqHEiGmN6Rlvczu58XwJsdqaesfAF477WiGb-tDxVHcxmGN2g' -X GET https://$ipaddr/info
+curl -f -k -H 'Accept: application/json' -H "Authorization: Bearer ${apikey}" -X GET https://$ipaddr/info
 
 echo "ivp/ensemble/device_list"
-curl -f -k -H 'Accept: application/json' -H 'Authorization: Bearer eyJraWQiOiI3ZDEwMDA1ZC03ODk5LTRkMGQtYmNiNC0yNDRmOThlZTE1NmIiLCJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJhdWQiOiI0ODI0MzcwMDczMDAiLCJpc3MiOiJFbnRyZXoiLCJlbnBoYXNlVXNlciI6Im93bmVyIiwiZXhwIjoxODAyMTkwMTE5LCJpYXQiOjE3NzA2NTQxMTksImp0aSI6ImNlZTFhZjI0LTU1MzQtNDZkYi04ZWM0LTNlZWVlMDM0YTgxNiIsInVzZXJuYW1lIjoibWFya21jaW50eXJlOTlAZ29vZ2xlbWFpbC5jb20ifQ.SJeIHOLW1B96ejeDimbR8yMCMcNhPTF6vLaIulqHEiGmN6Rlvczu58XwJsdqaesfAF477WiGb-tDxVHcxmGN2g' -X GET https://$ipaddr/ivp/ensemble/device_list
+# info on available devices, generally the Gateway and Battery
+curl -f -k -H 'Accept: application/json' -H "Authorization: Bearer ${apikey}" -X GET https://$ipaddr/ivp/ensemble/device_list
 
 echo "ivp/meters"
-curl -f -k -H 'Accept: application/json' -H 'Authorization: Bearer eyJraWQiOiI3ZDEwMDA1ZC03ODk5LTRkMGQtYmNiNC0yNDRmOThlZTE1NmIiLCJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJhdWQiOiI0ODI0MzcwMDczMDAiLCJpc3MiOiJFbnRyZXoiLCJlbnBoYXNlVXNlciI6Im93bmVyIiwiZXhwIjoxODAyMTkwMTE5LCJpYXQiOjE3NzA2NTQxMTksImp0aSI6ImNlZTFhZjI0LTU1MzQtNDZkYi04ZWM0LTNlZWVlMDM0YTgxNiIsInVzZXJuYW1lIjoibWFya21jaW50eXJlOTlAZ29vZ2xlbWFpbC5jb20ifQ.SJeIHOLW1B96ejeDimbR8yMCMcNhPTF6vLaIulqHEiGmN6Rlvczu58XwJsdqaesfAF477WiGb-tDxVHcxmGN2g' -X GET https://$ipaddr/ivp/meters
+# info on the available meters, generally production and net-consumption
+curl -f -k -H 'Accept: application/json' -H "Authorization: Bearer ${apikey}" -X GET https://$ipaddr/ivp/meters
+
 echo "ivp/meters/readings"
-curl -f -k -H 'Accept: application/json' -H 'Authorization: Bearer eyJraWQiOiI3ZDEwMDA1ZC03ODk5LTRkMGQtYmNiNC0yNDRmOThlZTE1NmIiLCJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJhdWQiOiI0ODI0MzcwMDczMDAiLCJpc3MiOiJFbnRyZXoiLCJlbnBoYXNlVXNlciI6Im93bmVyIiwiZXhwIjoxODAyMTkwMTE5LCJpYXQiOjE3NzA2NTQxMTksImp0aSI6ImNlZTFhZjI0LTU1MzQtNDZkYi04ZWM0LTNlZWVlMDM0YTgxNiIsInVzZXJuYW1lIjoibWFya21jaW50eXJlOTlAZ29vZ2xlbWFpbC5jb20ifQ.SJeIHOLW1B96ejeDimbR8yMCMcNhPTF6vLaIulqHEiGmN6Rlvczu58XwJsdqaesfAF477WiGb-tDxVHcxmGN2g' -X GET https://$ipaddr/ivp/meters/readings
+# meter readings from each of the PV array (production), grid (net-consumption) and battery
+curl -f -k -H 'Accept: application/json' -H "Authorization: Bearer ${apikey}" -X GET https://$ipaddr/ivp/meters/readings
+
 echo "ivp/meters/gridReading"
-curl -f -k -H 'Accept: application/json' -H 'Authorization: Bearer eyJraWQiOiI3ZDEwMDA1ZC03ODk5LTRkMGQtYmNiNC0yNDRmOThlZTE1NmIiLCJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJhdWQiOiI0ODI0MzcwMDczMDAiLCJpc3MiOiJFbnRyZXoiLCJlbnBoYXNlVXNlciI6Im93bmVyIiwiZXhwIjoxODAyMTkwMTE5LCJpYXQiOjE3NzA2NTQxMTksImp0aSI6ImNlZTFhZjI0LTU1MzQtNDZkYi04ZWM0LTNlZWVlMDM0YTgxNiIsInVzZXJuYW1lIjoibWFya21jaW50eXJlOTlAZ29vZ2xlbWFpbC5jb20ifQ.SJeIHOLW1B96ejeDimbR8yMCMcNhPTF6vLaIulqHEiGmN6Rlvczu58XwJsdqaesfAF477WiGb-tDxVHcxmGN2g' -X GET https://$ipaddr/ivp/meters/gridReading
+# reports power being drawn/exported to each phase of the grid
+curl -f -k -H 'Accept: application/json' -H "Authorization: Bearer ${apikey}" -X GET https://$ipaddr/ivp/meters/gridReading
 
 echo "ivp/pdm/energy"
-curl -f -k -H 'Accept: application/json' -H 'Authorization: Bearer eyJraWQiOiI3ZDEwMDA1ZC03ODk5LTRkMGQtYmNiNC0yNDRmOThlZTE1NmIiLCJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJhdWQiOiI0ODI0MzcwMDczMDAiLCJpc3MiOiJFbnRyZXoiLCJlbnBoYXNlVXNlciI6Im93bmVyIiwiZXhwIjoxODAyMTkwMTE5LCJpYXQiOjE3NzA2NTQxMTksImp0aSI6ImNlZTFhZjI0LTU1MzQtNDZkYi04ZWM0LTNlZWVlMDM0YTgxNiIsInVzZXJuYW1lIjoibWFya21jaW50eXJlOTlAZ29vZ2xlbWFpbC5jb20ifQ.SJeIHOLW1B96ejeDimbR8yMCMcNhPTF6vLaIulqHEiGmN6Rlvczu58XwJsdqaesfAF477WiGb-tDxVHcxmGN2g' -X GET https://$ipaddr/ivp/pdm/energy
+curl -f -k -H 'Accept: application/json' -H "Authorization: Bearer ${apikey}" -X GET https://$ipaddr/ivp/pdm/energy
 
 echo "ivp/livedata/status"
-curl -f -k -H 'Accept: application/json' -H 'Authorization: Bearer eyJraWQiOiI3ZDEwMDA1ZC03ODk5LTRkMGQtYmNiNC0yNDRmOThlZTE1NmIiLCJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJhdWQiOiI0ODI0MzcwMDczMDAiLCJpc3MiOiJFbnRyZXoiLCJlbnBoYXNlVXNlciI6Im93bmVyIiwiZXhwIjoxODAyMTkwMTE5LCJpYXQiOjE3NzA2NTQxMTksImp0aSI6ImNlZTFhZjI0LTU1MzQtNDZkYi04ZWM0LTNlZWVlMDM0YTgxNiIsInVzZXJuYW1lIjoibWFya21jaW50eXJlOTlAZ29vZ2xlbWFpbC5jb20ifQ.SJeIHOLW1B96ejeDimbR8yMCMcNhPTF6vLaIulqHEiGmN6Rlvczu58XwJsdqaesfAF477WiGb-tDxVHcxmGN2g' -X GET https://$ipaddr/ivp/livedata/status
+# shows live status of grid, PV, battery and system load, in watts
+curl -f -k -H 'Accept: application/json' -H "Authorization: Bearer ${apikey}" -X GET https://$ipaddr/ivp/livedata/status
 
 echo "api/v1/production"
-curl -f -k -H 'Accept: application/json' -H 'Authorization: Bearer eyJraWQiOiI3ZDEwMDA1ZC03ODk5LTRkMGQtYmNiNC0yNDRmOThlZTE1NmIiLCJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJhdWQiOiI0ODI0MzcwMDczMDAiLCJpc3MiOiJFbnRyZXoiLCJlbnBoYXNlVXNlciI6Im93bmVyIiwiZXhwIjoxODAyMTkwMTE5LCJpYXQiOjE3NzA2NTQxMTksImp0aSI6ImNlZTFhZjI0LTU1MzQtNDZkYi04ZWM0LTNlZWVlMDM0YTgxNiIsInVzZXJuYW1lIjoibWFya21jaW50eXJlOTlAZ29vZ2xlbWFpbC5jb20ifQ.SJeIHOLW1B96ejeDimbR8yMCMcNhPTF6vLaIulqHEiGmN6Rlvczu58XwJsdqaesfAF477WiGb-tDxVHcxmGN2g' -X GET https://$ipaddr/api/v1/production
-echo "api/v1/production/inverters"
-curl -f -k -H 'Accept: application/json' -H 'Authorization: Bearer eyJraWQiOiI3ZDEwMDA1ZC03ODk5LTRkMGQtYmNiNC0yNDRmOThlZTE1NmIiLCJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJhdWQiOiI0ODI0MzcwMDczMDAiLCJpc3MiOiJFbnRyZXoiLCJlbnBoYXNlVXNlciI6Im93bmVyIiwiZXhwIjoxODAyMTkwMTE5LCJpYXQiOjE3NzA2NTQxMTksImp0aSI6ImNlZTFhZjI0LTU1MzQtNDZkYi04ZWM0LTNlZWVlMDM0YTgxNiIsInVzZXJuYW1lIjoibWFya21jaW50eXJlOTlAZ29vZ2xlbWFpbC5jb20ifQ.SJeIHOLW1B96ejeDimbR8yMCMcNhPTF6vLaIulqHEiGmN6Rlvczu58XwJsdqaesfAF477WiGb-tDxVHcxmGN2g' -X GET https://$ipaddr/api/v1/production/inverters
+# shows PV production today, last 7 days, lifetime and instantaneously
+curl -f -k -H 'Accept: application/json' -H "Authorization: Bearer ${apikey}" -X GET https://$ipaddr/api/v1/production
 
-echo "inventory - includes battery"
-curl -f -k -H 'Accept: application/json' -H 'Authorization: Bearer eyJraWQiOiI3ZDEwMDA1ZC03ODk5LTRkMGQtYmNiNC0yNDRmOThlZTE1NmIiLCJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJhdWQiOiI0ODI0MzcwMDczMDAiLCJpc3MiOiJFbnRyZXoiLCJlbnBoYXNlVXNlciI6Im93bmVyIiwiZXhwIjoxODAyMTkwMTE5LCJpYXQiOjE3NzA2NTQxMTksImp0aSI6ImNlZTFhZjI0LTU1MzQtNDZkYi04ZWM0LTNlZWVlMDM0YTgxNiIsInVzZXJuYW1lIjoibWFya21jaW50eXJlOTlAZ29vZ2xlbWFpbC5jb20ifQ.SJeIHOLW1B96ejeDimbR8yMCMcNhPTF6vLaIulqHEiGmN6Rlvczu58XwJsdqaesfAF477WiGb-tDxVHcxmGN2g' -X GET https://$ipaddr/ivp/ensemble/inventory
+echo "api/v1/production/inverters"
+# shows what each microinverter is doing
+curl -f -k -H 'Accept: application/json' -H "Authorization: Bearer ${apikey}" -X GET https://$ipaddr/api/v1/production/inverters
+
+echo "inventory"
+# inventory of storage devices ie batteries
+curl -f -k -H 'Accept: application/json' -H "Authorization: Bearer ${apikey}" -X GET https://$ipaddr/ivp/ensemble/inventory
